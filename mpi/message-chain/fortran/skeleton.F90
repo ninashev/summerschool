9b53930abd62675179cedfa2389c97e694dba7b9
nina <ninashev@seznam.cz>
2019-06-29 14:35:33 +0200

    mpi exercises after installing openmpi - has mpi_f08, not all!

--- mpi/message-chain/fortran/skeleton.F90
+++ mpi/message-chain/fortran/skeleton.F90
@@ -1,5 +1,5 @@
 program basic
-  use mpi
+  use mpi_f08
   use iso_fortran_env, only : REAL64
 
   implicit none
@@ -7,7 +7,8 @@ program basic
   integer :: rc, myid, ntasks, nrecv,nsend
   integer :: message(msgsize)
   integer :: receiveBuffer(msgsize)
-  type(mpi_status) :: status
+  type(mpi_status) :: status1,status2
+  type(mpi_request) :: request1, request2
 
   real(REAL64) :: t0, t1
 
@@ -22,36 +23,58 @@ program basic
   call mpi_barrier(mpi_comm_world, rc)
   t0 = mpi_wtime()
 
-  nsend = myid + 1
-  nrecv = myid - 1
-  if (nsend > ntasks - 1) then
-  nsend = mpi_proc_null
-  endif
-  if (nrecv < 0) then
-  nrecv = mpi_proc_null
-  endif
+  !nsend = myid + 1
+  !nrecv = myid - 1
+  !if (nsend > ntasks - 1) then
+  !nsend = mpi_proc_null
+  !endif
+  !if (nrecv < 0) then
+  !nrecv = mpi_proc_null
+  !endif
   
-!  if (myid > 0 .AND. myid < ntasks-1) then
-     call mpi_sendrecv(message,msgsize,mpi_integer,nsend,myid+1, &
-     receivebuffer,msgsize,mpi_integer,nrecv,myid,mpi_comm_world,status,rc)
-!  endif
+ 
+  !   call mpi_sendrecv(message,msgsize,mpi_integer,nsend,myid+1, &
+  !        receivebuffer,msgsize,mpi_integer,nrecv,myid,mpi_comm_world,status,rc)
+     
   !print *,'status = ',status  
+
+  request1 = mpi_request_null
+  request2 = mpi_request_null
 !---------------------------------------------------------------  
   ! TODO: Send and receive as defined in the assignment
- ! if (myid < ntasks-1) then
-  !   nrecv = myid+1
-  !   call mpi_send(message,msgsize,mpi_integer,nrecv,nrecv,mpi_comm_world,rc)
-     write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
+  if (myid < ntasks-1) then
+     nrecv = myid+1
+!!!call mpi_send(message,msgsize,mpi_integer,nrecv,nrecv,mpi_comm_world,rc)
+     call mpi_isend(message,msgsize,mpi_integer,nrecv,nrecv,mpi_comm_world,request1,rc)
+  !   call mpi_wait(request1,status1,rc)
+  !   write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
+  !        ' Sent elements: ', msgsize, &
+  !        '. Tag: ', myid+1, '. Receiver: ', myid+1
+  end if
+   
+  if (myid > 0) then
+     nsend = myid - 1
+!!!call mpi_recv(receivebuffer,msgsize,mpi_integer,nsend,nsend+1,mpi_comm_world,status,rc)
+     call mpi_irecv(receivebuffer,msgsize,mpi_integer,nsend,nsend+1,mpi_comm_world,request2,rc)
+ !    call mpi_wait(request2,status2,rc)
+ !    write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
+ !         ' First element: ', receiveBuffer(1)
+  end if
+
+  
+  !call mpi_wait(request1,mpi_status_ignore,rc)
+  call mpi_wait(request1,status1,rc)
+  call mpi_wait(request2,status2,rc)
+   ! if(myid < ntasks-1)
+       write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
           ' Sent elements: ', msgsize, &
           '. Tag: ', myid+1, '. Receiver: ', myid+1
-  !end if
-   
-  !if (myid > 0) then
-  !   nsend = myid - 1
-  !   call mpi_recv(receivebuffer,msgsize,mpi_integer,nsend,nsend+1,mpi_comm_world,status,rc)
-     write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
-          ' First element: ', receiveBuffer(1)
-  !end if
+   ! endif
+   ! if (myid > 0) then
+       write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
+            ' First element: ', receiveBuffer(1)
+   ! endif
+    
 
   ! Finalize measuring the time and print it out
   t1 = mpi_wtime()
