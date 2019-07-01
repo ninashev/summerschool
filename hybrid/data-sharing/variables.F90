0196f62f715b8ca7cb4f9c5ad12cfac046874919
nina <ninashev@seznam.cz>
2019-07-01 17:49:56 +0200

    first exercises in /summerschool/hybrid/ are dne

--- hybrid/data-sharing/variables.F90
+++ hybrid/data-sharing/variables.F90
@@ -1,16 +1,49 @@
 program exer1
+  use mpi_f08
+  use omp_lib
   implicit none
   integer :: var1, var2
+  integer :: provided, ierr
+  
   var1 = 1
   var2 = 2
 
-  ! TODO:
-  !   Test different data sharing clauses here
+!$omp parallel default(shared) private(var1,var2)
+ ! TODO:                                                                        !   Test different data sharing clauses here                                                      
   print *, 'Region 1:       var1=', var1, 'var2=', var2
   var1 = var1 + 1
   var2 = var2 + 1
-  !end here :)
+  ! end here :) 
+
+!$omp end parallel
+  
   print *, 'After region 1: var1=', var1, 'var2=', var2
   print *
+!--
 
+!$omp parallel default(shared) firstprivate(var1,var2)                                                       
+ ! TODO:                                                                       
+ !   Test different data sharing clauses here                                                      
+  print *, 'Region 2:       var1=', var1, 'var2=', var2
+  var1 = var1 + 1
+  var2 = var2 + 1
+  ! end here :)                                                                                     
+!$omp end parallel                                                                                 
+  print *, 'After region 2: var1=', var1, 'var2=', var2
+  print *
+!--
+
+!$omp parallel default(shared)                                  
+ ! TODO:     
+ !   Test different data sharing clauses here                                                      
+  print *, 'Region 3:       var1=', var1, 'var2=', var2
+  var1 = var1 + 1
+  var2 = var2 + 1
+  ! end here :)       
+
+!$omp end parallel                                                                                  
+  print *, 'After region 3: var1=', var1, 'var2=', var2
+  print *
+  
+  
 end program exer1
